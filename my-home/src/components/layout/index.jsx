eceb44b722e79b44e1a15efd07ed4d1f56d12ea9
maxmutov-nuriddin <mahmudovnuriddin35@gmail.com>
2023-11-01 19:28:54 +0500

    update my-home/src/components/layout/index.jsx

--- my-home/src/components/layout/index.jsx
+++ my-home/src/components/layout/index.jsx
@@ -40,12 +40,60 @@ const Layouts = () => {
   return (
     <Layout className='admin-layout'>
       <Sider trigger={null} collapsible collapsed={collapsed}>
-      <Link className="demo-logo-vertical" to='/'>MY HOME</Link>
+        <Link className="demo-logo-vertical" to='/'>MY HOME</Link>
         <Menu
           theme="dark"
           mode="inline"
           defaultSelectedKeys={[location.pathname]}
-          items={items}
+          // items={items}
+          items={[
+            {
+              key: '2015',
+              icon: <CalendarOutlined />,
+              label: '2015',
+            },
+            {
+              key: '2016',
+              icon: <CalendarOutlined />,
+              label: '2016',
+            },
+            {
+              key: '2017',
+              icon: <CalendarOutlined />,
+              label: '2017',
+            },
+            {
+              key: '2018',
+              icon: <CalendarOutlined />,
+              label: '2018',
+            },
+            {
+              key: '2019',
+              icon: <CalendarOutlined />,
+              label: '2019',
+            },
+            {
+              key: '2020',
+              icon: <CalendarOutlined />,
+              label: '2020',
+            },
+            {
+              key: '2021',
+              icon: <CalendarOutlined />,
+              label: '2021',
+            },
+            {
+              key: '2022',
+              icon: <CalendarOutlined />,
+              label: '2022',
+            },
+            {
+              key: '2023',
+              icon: <CalendarOutlined />,
+              label: '2023',
+            },
+          ]}
+
         />
       </Sider>
       <Layout>
